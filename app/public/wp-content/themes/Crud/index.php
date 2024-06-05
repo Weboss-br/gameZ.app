b2b9955c1933af102ff5e3113f677d13c9bd8796
Suporte.TI <Suporte.TI@Dieter-SuporteTI.local>
2024-06-05 13:25:01 -0300

    melhoramento do visual

--- app/public/wp-content/themes/Crud/index.php
+++ app/public/wp-content/themes/Crud/index.php
@@ -1,10 +1,25 @@
-<?php get_header(); ?>
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>CRUD de Itens</title>
+    <?php wp_head(); ?>
+</head>
+<body>
+    <?php get_header(); ?>
 
-<h1>Bem-vindo ao CRUD de Itens</h1>
-<h2>Criar um novo item</h2>
-<?php echo do_shortcode('[custom_form]'); ?>
+    <h1 style="color: #333; font-family: Arial, sans-serif;">Bem-vindo ao CRUD de Itens</h1>
 
-<h2>Lista de itens</h2>
-<?php echo do_shortcode('[list_items]'); ?>
+    <div style="border-radius: 5px; background-color: #f2f2f2; padding: 40px; font-family: Arial, sans-serif;">
+        <h2 style="color: #333;">Criar um novo item</h2>
+        <?php echo do_shortcode('[custom_form]'); ?>
+    </div>
 
-<?php get_footer(); ?>
+    <h2 style="color: #333; font-family: Arial, sans-serif;">Lista de itens</h2>
+    <?php echo do_shortcode('[list_items]'); ?>
+
+    <?php get_footer(); ?>
+    <?php wp_footer(); ?>
+</body>
+</html>
